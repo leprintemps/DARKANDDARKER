ab7760386ef2ac14d567a74726d26b7b64706312
leprintemps <leprintemps7637@gmail.com>
2023-01-11 19:35:29 +0900

    aa

--- server/src/schema/board.schema.ts
+++ server/src/schema/board.schema.ts
@@ -1,48 +1,35 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { Blog } from './blog.schema';
 
-export interface postField {
-    createdAt: Date,
-    updatedAt: Date,
-    title: string,
-    author: string,
-    body: string,
-    comments: Post[],
+export interface boardField {
+  title: string,
+  description: string,
+  posts: post[]
+  createAt: Date,
+  updateAt: Date,
 }
 
-// MongoDB의 가장 작은 단위가 Document, 모듈에서 사용할 타입을 export 시켜줌
-export type PostDocument = HydratedDocument<Post>;
+export type BoardDocument = HydratedDocument<Board>;
 
-// 아래와 같이 timestamp 설정도 가능하다.
-// createdAt과 updatedAt둘 중에 하나만 사용하고 싶다면 아래와 같이 작성도 가능하다.
-// @Schema({ timestamps: { createdAt: "createdAt", updatedAt: false } })
 @Schema({ timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } })
-export class Post implements postField{
+export class Board implements boardField{
+  
+  @Prop({ type: String, required: true, maxlength: 30 })
+  title: string;
+
+  @Prop({ type: String, required: true, maxlength: 30 })
+  description: string;
+  
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Blog' })
+  blog: Blog;
 
   @Prop({ default: new Date(), type: mongoose.Schema.Types.Date })
-  createdAt: Date;
+  createAt: Date;
 
   @Prop({ default: new Date(), type: mongoose.Schema.Types.Date })
-  updatedAt: Date;
+  updateAt: Date;
 
-  @Prop({ type: String, required: true, trim: true, maxLength: 30 }) // MongoDB에 들어갈 설정들을 적어준다.
-  title: string; // 필드 이름: 타입(타입스크립트 타입)
-
-  @Prop({ type: String, required: true, trim: true })
-  author: string;
-
-  @Prop({ type: String, required: true })
-  body: string;
-  
-  
-  // object를 저장하는 것도 가능하다.
-  @Prop({type:{
-	  id: { required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-      uuid: { required: true, type: String },
-  }})
-  comments: Post[];
- 
 }
 
-// 위의 작성한 클래스를 바탕으로 Mongoose에서 사용하는 스키마 클래스를 만들어준다.
-export const PostSchema = SchemaFactory.createForClass(Post);
+export const BoardSchema = SchemaFactory.createForClass(Board);
