2bfe7d4e9ad722ef66895b404188773e27efbbd3
leprintemps <leprintemps7637@gmail.com>
2023-01-25 23:20:53 +0900

    ㅁㅁ

--- client/pages/signin.tsx
+++ client/pages/signin.tsx
@@ -1,90 +1,134 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Container, Grid, Box, Typography, Stack, Link as MuiLink, FormControlLabel, Checkbox } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import { literal, object, string, TypeOf } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import FormInput from "../components/Forminput";
+import styled from '@emotion/styled';
+import GoogleLogo from "../assets/google.svg";
+import GitHubLogo from "../assets/github.svg";
 import { useAppDispatch } from '../config/redux/hooks';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 import { userSigninAsync } from '../requests/user/userSlice';
+import Copyright from '../components/Copyright';
+import Link from 'next/link';
 
-function Copyright(props: any) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">TOMODDATZZI</Link>
-            {' '}{new Date().getFullYear()}{'.'}
-        </Typography>
-    );
-}
+export const OauthMuiLink = styled(MuiLink)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f5f6f7;
+    border-radius: 1;
+    padding: 0.6rem 0;
+    column-gap: 1rem;
+    text-decoration: none;
+    color: #393e45;
+    font-weight: 500;
+    cursor: pointer;
+    &:hover {
+        background-color: #fff;
+        box-shadow: 0 1px 13px 0 rgb(0 0 0 / 15%);
+    }
+`;
 
-const theme = createTheme();
+export const LinkItem = styled(Link)`
+  text-decoration: none;
+  color: #3683dc;
+  &:hover {
+    text-decoration: underline;
+    color: #5ea1b6;
+  }
+`;
+
+const signinSchema = object({
+    username: string()
+        .min(1, "Username is required.")
+        .max(30),
+    password: string()
+        .min(1, "Password is required.")
+        .min(8, "Password must be more than 8 characters.")
+        .max(32, "Password must be less than 32 characters."),
+    persistUser: literal(true)
+        .optional(),
+});
+
+type IsSignin = TypeOf<typeof signinSchema>;
 
 export default function Signin() {
 
     const dispatch = useAppDispatch();
     const router = useRouter();
 
-    const [user, setUser] = useState({
+    const defaultValues: IsSignin = {
         username: "",
         password: "",
-    })
+    };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
+    const methods = useForm<IsSignin>({
+        resolver: zodResolver(signinSchema),
+        defaultValues,
+    });
 
-        setUser({
-            ...user,
-            [name]: value,
-        })
-    }
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        dispatch(userSigninAsync(user))
+    const onSubmitHandler: SubmitHandler<IsSignin> = (data: IsSignin) => {
+        dispatch(userSigninAsync(data))
         .then(() => {
-            router.push("/");
+            router.push("/")
         })
         .catch((reason) => {
+            alert("오류발생!!!");
             console.log(reason);
         })
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">Sign in</Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField margin="normal" id="username" name="username" label="Username" autoComplete="username" onChange={handleChange} autoFocus required fullWidth />
-                        <TextField margin="normal" id="password" name="password" label="Password" type="password"  autoComplete="password" onChange={handleChange} required fullWidth />
-                        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me"/>
-                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign In</Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">Forgot password?</Link>
+        <Container maxWidth={false} sx={{ height: '100vh', backgroundColor: { xs: '#fff', md: '#f4f4f4' } }}>
+            <Grid container justifyContent='center' alignItems='center' sx={{ width: '100%', height: '100%' }}>
+                <Grid item sx={{ maxWidth: '70rem', width: '100%', backgroundColor: '#fff' }}>
+                    <FormProvider {...methods}>
+                        <Grid container sx={{ boxShadow: { sm: '0 0 5px #ddd' }, py: '6rem', px: '1rem', }}>
+                            <Grid item container justifyContent='space-between' rowSpacing={5} sx={{ maxWidth: { sm: '45rem' }, marginInline: 'auto', }}>
+                                <Grid item xs={12} sm={6} sx={{ borderRight: { sm: '1px solid #ddd' } }}>
+                                    <Box display='flex' flexDirection='column' component='form' noValidate autoComplete='off' sx={{ paddingRight: { sm: '3rem' } }} onSubmit={methods.handleSubmit(onSubmitHandler)}>
+                                        <Typography variant='h6' component='h1' sx={{ textAlign: 'center', mb: '1.5rem' }}>Log into your account</Typography>
+
+                                        <FormInput label='Username' type='text' name='username' focused required/>
+                                        <FormInput label='Password' type='password' name='password' focused required/>
+
+                                        <FormControlLabel control={ <Checkbox size='small' aria-label='trust this device checkbox' required {...methods.register('persistUser')}/>} label={ <Typography variant='body2' sx={{ fontSize: '0.8rem', fontWeight: 400, color: '#5e5b5d', }}> Trust this device </Typography>}/>
+
+                                        <LoadingButton loading={false} type='submit' variant='contained' sx={{ py: '0.8rem', mt: 2, width: '80%', marginInline: 'auto', }}>Login</LoadingButton>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <Typography variant='h6' component='p' sx={{ paddingLeft: { sm: '3rem' }, mb: '1.5rem', textAlign: 'center', }}>Log in with another provider:</Typography>
+                                    <Box display='flex' flexDirection='column' sx={{ paddingLeft: { sm: '3rem' }, rowGap: '1rem' }}>
+                                        <OauthMuiLink href=''>
+                                            <GoogleLogo style={{ height: '2rem' }} />
+                                            Google
+                                        </OauthMuiLink>
+                                        <OauthMuiLink href=''>
+                                            <GitHubLogo style={{ height: '2rem' }} />
+                                            GitHub
+                                        </OauthMuiLink>
+                                    </Box>
+                                </Grid>
                             </Grid>
-                            <Grid item>
-                                <Link href="/signup" variant="body2">{"Don't have an account? Sign Up"}</Link>
+                            <Grid container justifyContent='center'>
+                                <Stack sx={{ mt: '3rem', textAlign: 'center' }}>
+                                    <Typography sx={{ fontSize: '0.9rem', mb: '1rem' }}>Need an account?{' '}
+                                        <LinkItem href="/signup">Sign up here</LinkItem>
+                                    </Typography>
+                                    <Typography sx={{ fontSize: '0.9rem' }}>
+                                        Forgot your{' '}
+                                        <LinkItem href="forgotPassword">password?</LinkItem>
+                                    </Typography>
+                                </Stack>
                             </Grid>
                         </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
+                    </FormProvider>
+                </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+        </Container>
     );
-}
+};
--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,107 +1,139 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
+import { Container, Grid, Box, Typography, Stack, Link as MuiLink } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import { object, string, TypeOf } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import FormInput from "../components/Forminput";
+import styled from '@emotion/styled';
+import GoogleLogo from "../assets/google.svg";
+import GitHubLogo from "../assets/github.svg";
 import { useAppDispatch } from '../config/redux/hooks';
 import { useRouter } from 'next/router';
 import { userSignupAsync } from '../requests/user/userSlice';
+import Copyright from '../components/Copyright';
+import { LinkItem } from './signin';
 
-function Copyright(props: any) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">TOMODDATZZI</Link>
-            {' '}{new Date().getFullYear()}{'.'}
-        </Typography>
-    );
-}
+export const OauthMuiLink = styled(MuiLink)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f5f6f7;
+    border-radius: 1;
+    padding: 0.6rem 0;
+    column-gap: 1rem;
+    text-decoration: none;
+    color: #393e45;
+    font-weight: 500;
+    cursor: pointer;
+    &:hover {
+        background-color: #fff;
+        box-shadow: 0 1px 13px 0 rgb(0 0 0 / 15%);
+    }
+`;
 
-const theme = createTheme();
+const signupSchema = object({
+    username: string()
+        .min(1, "Username is required.")
+        .max(30),
+    password: string()
+        .min(1, "Password is required.")
+        .min(8, "Password must be more than 8 characters.")
+        .max(32, "Password must be less than 32 characters."),
+    password_confirm: string()
+        .min(1, "Please confirm your password."),
+    name: string()
+        .min(1, "Name is required.")
+        .max(30),
+    email: string()
+        .min(1, "Email is required.")
+        .max(100),
+    location: string()
+        .max(30),
+}).refine((data) => data.password === data.password_confirm, {
+    path: ["password_confirm"],
+    message: "Passwords do not match.",
+});
+
+type IsSignup = TypeOf<typeof signupSchema>;
 
 export default function Signup() {
 
     const dispatch = useAppDispatch();
     const router = useRouter();
 
-    const [user, setUser] = useState({
+    const defaultValues: IsSignup = {
         username: "",
         password: "",
         name: "",
         email: "",
         location: "",
-    })
+        password_confirm: "",
+    };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
+    const methods = useForm<IsSignup>({
+        resolver: zodResolver(signupSchema),
+        defaultValues,
+    });
 
-        setUser({
-            ...user,
-            [name]: value,
-        })
-    }
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        dispatch(userSignupAsync(user))
+    const onSubmitHandler: SubmitHandler<IsSignup> = (data: IsSignup) => {
+        dispatch(userSignupAsync(data))
         .then(() => {
-            router.push("/");
+            router.push("/")
         })
         .catch((reason) => {
+            alert("오류발생!!!");
             console.log(reason);
         })
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                <Typography component="h1" variant="h5">Sign up</Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField autoComplete="username" name="username" id="username" label="Username" onChange={handleChange} autoFocus required fullWidth />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField autoComplete="password" name="password" id="password" label="Password" onChange={handleChange} autoFocus required fullWidth />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField autoComplete="name" name="name" id="name" label="Name" onChange={handleChange} autoFocus required fullWidth />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField autoComplete="email" name="email" id="email" label="Email Address" onChange={handleChange} autoFocus required fullWidth />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField autoComplete="location" name="location" id="location" label="Location" onChange={handleChange} autoFocus fullWidth />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel control={<Checkbox value="allowExtraEmails" color="primary" />} label="I want to receive inspiration, marketing promotions and updates via email." />
-                            </Grid>
-                        </Grid>
-                    <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign Up</Button>
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            <Link href="/signin" variant="body2">Already have an account? Sign in</Link>
+        <Container maxWidth={false} sx={{ height: '100vh', backgroundColor: { xs: '#fff', md: '#f4f4f4' } }}>
+            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Grid container justifyContent='center' alignItems='center' sx={{ width: '100%', height: '100%' }}>
+                    <Grid item sx={{ maxWidth: '70rem', width: '100%', backgroundColor: '#fff' }}>
+                        <Grid container sx={{ boxShadow: { sm: '0 0 5px #ddd' }, py: '6rem', px: '1rem',}}>
+                            <FormProvider {...methods}>
+                                <Typography variant='h4' component='h1' sx={{ textAlign: 'center', width: '100%', mb: '1.5rem', pb: { sm: '3rem' } }}>Welcome To TOMODDATZZI!</Typography>
+                                <Grid item container justifyContent='space-between' rowSpacing={5} sx={{ maxWidth: { sm: '45rem' }, marginInline: 'auto' }}>
+                                    <Grid item xs={12} sm={6} sx={{ borderRight: { sm: '1px solid #ddd' } }}>
+                                    <Box display='flex' flexDirection='column' component='form' noValidate autoComplete='off' sx={{ paddingRight: { sm: '3rem' } }} onSubmit={methods.handleSubmit(onSubmitHandler)}>
+                                        <Typography variant='h6' component='h1' sx={{ textAlign: 'center', mb: '1.5rem' }}>Create new your account</Typography>
+
+                                        <FormInput label='Username' type='text' name='username' focused required/>
+                                        <FormInput label='Password' type='password' name='password' required focused/>
+                                        <FormInput label='Password Confirm' type='password' name='password_confirm' required focused/>
+                                        <FormInput label='Email' type='email' name='email' focused required/>
+                                        <FormInput label='Name' type='text'  name='name' required focused/>
+                                        <FormInput label='Location' type='location'  name='location' focused/>
+
+                                        <LoadingButton loading={false} type='submit' variant='contained' sx={{ py: '0.8rem', mt: 2, width: '80%', marginInline: 'auto',}}>Sign Up</LoadingButton>
+                                    </Box>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6} sx={{}}>
+                                        <Typography variant='h6' component='p' sx={{ paddingLeft: { sm: '3rem' }, mb: '1.5rem', textAlign: 'center',}}>Sign up using another provider:</Typography>
+                                        <Box display='flex' flexDirection='column' sx={{ paddingLeft: { sm: '3rem' }, rowGap: '1rem' }}>
+                                            <OauthMuiLink href=''>
+                                                <GoogleLogo style={{ height: '2rem' }} />
+                                                Google
+                                            </OauthMuiLink>
+                                            <OauthMuiLink href=''>
+                                                <GitHubLogo style={{ height: '2rem' }} />
+                                                GitHub
+                                            </OauthMuiLink>
+                                        </Box>
+                                    </Grid>
+                                </Grid>
+                                <Grid container justifyContent='center'>
+                                    <Stack sx={{ mt: '3rem', textAlign: 'center' }}>
+                                        <Typography sx={{ fontSize: '0.9rem', mb: '1rem' }}>Already have an account? <LinkItem href="/signin">Login</LinkItem></Typography>
+                                    </Stack>
+                                </Grid>
+                            </FormProvider>
                         </Grid>
                     </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 5 }} />
-            </Container>
-        </ThemeProvider>
+                </Grid>
+            </Box>
+            <Copyright sx={{ mt: 5 }} />
+        </Container>
     );
-}
+};
